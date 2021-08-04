7aa715b8e4b301176945649ecf309c0a8d3f9401
yeonatalie <yeonatalie@yahoo.com.sg>
2021-08-04 14:06:58 +0800

    Checkout function implemented

--- qrapp/src/views/Cart.vue
+++ qrapp/src/views/Cart.vue
@@ -18,18 +18,30 @@
         </ion-toolbar>
         <ion-list>
           <ion-item v-for="(item, index) in cartList" :key="index">
-            <ion-label>{{item.title}}</ion-label>
-            <ion-label>{{item.quantity}}</ion-label>
+            <ion-thumbnail slot="start">
+              <img class="image" :src="item.img">
+            </ion-thumbnail>
+            <ion-label>
+              <h2>{{item.title}}</h2>
+              <p>${{item.dollar}} or {{item.points}} points</p>
+              <p>Quanitity: {{item.quantity}}</p>
+            </ion-label>
           </ion-item>
+          <ion-item>
+            <h4>Total: </h4> 
+            <h5>${{totalDollar}} or {{totalPoints}} points</h5>
+          </ion-item> 
         </ion-list>
       </ion-header>
+      <ion-button id='checkoutButton' @click="checkout">Check Out</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, alertController} from '@ionic/vue';
 import { db } from "@/main";
+import firebase from "firebase";
 
 export default  {
   name: 'Cart',
@@ -37,6 +49,11 @@ export default  {
   data() {
     return {
       cartList: [],
+      totalDollar: 0,
+      totalPoints: 0,
+      userWallet: null,
+      totalCost: null,
+      walletErrorMsg: "",
     }
   },
   methods: {
@@ -54,37 +71,218 @@ export default  {
             ref.get().then(snapshot => {
               if (snapshot.exists) {
                 const refId = ref.id
-                const title = snapshot.data().name
-                const dollar = snapshot.data().costDollar
-                const points = snapshot.data().costPoints
-                if (refList.includes(refId)) {
-                  const i = refList.findIndex(function(e) {return e == refId})
-                  this.cartList[i] = {
-                    title: title,
-                    dollar: dollar,
-                    points: points,
-                    quantity: (this.cartList[i].quantity +1)
+                const merchRef = snapshot.data().merchant
+                merchRef.get().then(s => {
+                  if (s.exists) {
+                    const merchantName = s.data().name
+                    const title = snapshot.data().name + " " + merchantName
+                    const img = snapshot.data().image
+                    const dollar = snapshot.data().costDollar
+                    const points = snapshot.data().costPoints
+                    this.totalDollar += dollar
+                    this.totalPoints += points
+                    if (refList.includes(refId)) {
+                      const i = refList.findIndex(function(e) {return e == refId})
+                      const qty = (this.cartList[i].quantity +1)
+                      this.cartList[i] = {
+                        voucherTypeRef: ref,
+                        title: title,
+                        img: img,
+                        quantity: qty,
+                        dollar: dollar * qty,
+                        points: points * qty,
+                      }
+                    } else {
+                      this.cartList.push({
+                        voucherTypeRef: ref,
+                        title: title,
+                        img: img,
+                        dollar: dollar,
+                        points: points,
+                        quantity: 1
+                      })
+                      refList.push(refId)
+                    }
                   }
-                } else {
-                  this.cartList.push({
-                    title: title,
-                    dollar: dollar,
-                    points: points,
-                    quantity: 1
-                  })
-                  refList.push(refId)
-                  console.log(refList)
-                }
+                })
               }
             })
           })
-
-          console.log('$$$')
-          console.log(this.cartList)
-          console.log('$$$')
         }
       })
-    }
+    },
+    checkout: function() {
+      console.log('checkout')
+      this.alertCheckoutMethod()
+    },
+    checkQty: async function() {
+      let qtyFail = false
+      console.log('# start checking qty')
+      await Promise.all(this.cartList.map(async (ele) => {
+        await ele.voucherTypeRef.get().then(snapshot => {
+          if (snapshot.exists) {
+            const qtyLeft = snapshot.data().count
+            if (qtyLeft < ele.quantity) {
+              this.qtyErrorAlert(qtyLeft, ele.title)
+              qtyFail = true
+            }
+            console.log('%%%')
+          }
+        })
+      }))
+      console.log('returning from checkQty')
+      return qtyFail
+    },
+    checkoutQtyConfirmation: async function(purchaseMethod) {
+      const qtyFail = await this.checkQty()
+      console.log(qtyFail)
+      if (!qtyFail) {
+        console.log('### CONFIRMATION METHOD CALLED')
+        this.checkoutConfirmation(purchaseMethod)
+      }
+    },
+    checkoutConfirmation: function(purchaseMethod) {
+      // check enough cash / points
+      if (this.totalCost > this.userWallet) {
+        this.walletErrorAlert(purchaseMethod, this.userWallet)
+      } 
+      else { // transaction successful. update user wallet balance and clear cart. update voucherType count. create userVoucher instance
+        console.log('transaction successful!')
+        if (purchaseMethod == '$') {
+          db.collection('user')
+          .doc('4AGK7K5pWEtTSidHcpL3') // HARDCODE TO CHANGE
+          .update({
+            cart: [],
+            walletBalanceDollar: firebase.firestore.FieldValue.increment(- this.totalCost),
+          })
+        } else if (purchaseMethod == "points") {
+          db.collection('user')
+          .doc('4AGK7K5pWEtTSidHcpL3') // HARDCODE TO CHANGE
+          .update({
+            cart: [],
+            walletBalancePoints: firebase.firestore.FieldValue.increment(- this.totalCost),
+          })
+        }
+        this.cartList.forEach(ele => {
+          console.log('ELE VOUCHER NAME')
+          console.log(ele.title)
+          console.log(ele.quantity)
+          console.log('###')
+          for (let i=1; i <= ele.quantity; i++) {
+            // create userVoucher instance
+            const userVoucherId = db.collection('userVoucher').doc().id;
+            db.collection('userVoucher').doc(userVoucherId)
+            .set({
+              createdAt: new Date(),
+              userRef: db.doc('user/' + '4AGK7K5pWEtTSidHcpL3'), // HARDCODE TO CHANGE
+              voucherTypeRef: db.doc('voucherType/' + ele.voucherTypeRef.id),
+              paymentType: purchaseMethod
+            });
+          }
+          ele.voucherTypeRef.update({
+            count: firebase.firestore.FieldValue.increment(- ele.quantity),
+          })
+        })
+        this.checkoutConfirmationAlert()
+      }
+    },
+    async alertCheckoutMethod() {
+      const alert = await alertController
+        .create({
+          cssClass: 'my-custom-class',
+          header: 'Purchase Options',
+          //subHeader: 'Subtitle',
+          //message: 'This is an alert message.',
+          buttons: [
+            {
+              text: 'Cash',
+              handler: () => {
+                console.log('Cash Chosen')
+                db.collection('user')
+                .doc('4AGK7K5pWEtTSidHcpL3') // HARDCODE TO CHANGE
+                .get()
+                .then(documentSnapshot => {
+                  if (documentSnapshot.exists) {
+                    this.userWallet = documentSnapshot.data().walletBalanceDollar
+                    this.totalCost = this.totalDollar
+                    this.checkoutQtyConfirmation('$')
+                  }
+                })
+              }
+            }, 
+            {
+              text: 'Points',
+              handler: () => {
+                console.log('Points Chosen')
+                db.collection('user')
+                .doc('4AGK7K5pWEtTSidHcpL3') // HARDCODE TO CHANGE
+                .get()
+                .then(documentSnapshot => {
+                  if (documentSnapshot.exists) {
+                    this.userWallet = documentSnapshot.data().walletBalancePoints
+                    this.totalCost = this.totalPoints
+                    this.checkoutQtyConfirmation('points')
+                  }
+                })
+              }
+            }
+          ],
+        });
+      return alert.present();
+    },
+    qtyErrorAlert: async function (qty, title) {
+      console.log('Sold Out')
+      const alert = await alertController
+        .create({
+          cssClass: 'my-custom-class',
+          header: 'Sold Out',
+          message: 'There are only ' + qty + ' "' + title + '" vouchers available for purchase',
+          buttons: [{
+            text: 'Ok',
+          }],
+        });
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+    },
+    walletErrorAlert: async function (type, balance) {
+      console.log('Not enough $ / points')
+      if (type == "$") {
+        this.walletErrorMsg = "You only have $" + balance + " reamining in your wallet. Please top up your wallet to checkout." 
+      } else if (type == "points") {
+        this.walletErrorMsg = "You only have " + balance + " points left. Please accumulate more points or checkout using cash." 
+      }
+      const alert = await alertController
+        .create({
+          cssClass: 'my-custom-class',
+          header: 'Insufficient Wallet Balance',
+          message: this.walletErrorMsg,
+          buttons: [{
+            text: 'Ok',
+          }],
+        });
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+    },
+    checkoutConfirmationAlert: async function () {
+      console.log('Successfully checked out')
+      const alert = await alertController
+        .create({
+          cssClass: 'my-custom-class',
+          header: 'Successfully',
+          message: 'The voucher(s) have been purchased. You may view them in your wallet.',
+          buttons: [{
+            text: 'Ok',
+            handler: () => {
+              this.$router.go(); //REFRESH PAGE
+            }
+          }],
+        });
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+    },
   },
   created() {
     this.fetchItems();
@@ -105,5 +303,23 @@ export default  {
 }
 ion-title {
   left: 35vw;
+}
+ion-thumbnail {
+  height: 80px;
+  width: 80px;
+  --border-radius: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px
+}
+ion-item {
+  padding: 10px 0 ;
+}
+h5 {
+  margin-left: 10px;
+}
+#checkoutButton {
+  position: absolute;
+  bottom: 70px;
+  right: 30px;
 }
 </style>
